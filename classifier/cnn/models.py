5417de1e3e34028a67216a3d9bea27b4f04b86da
Laurent Vanni <lvanni@unice.fr>
2019-01-25 15:01:12 +0100

    NEW: introduce the multi channels syntax

--- classifier/cnn/models.py
+++ classifier/cnn/models.py
@@ -26,7 +26,8 @@ class CNNModel:
 		print("-"*20)
 		print("CREATE MODEL")
 		print("-"*20)
-		
+
+		"""
 		inputs = Input(shape=(config["SEQUENCE_SIZE"],), dtype='int32')
 		
 		# ---------------
@@ -69,6 +70,51 @@ class CNNModel:
 		print("MaxPooling2D :", maxpool.shape)
 		maxpool = Flatten()(maxpool)
 		print("flatten :", maxpool.shape)
+		"""
+		
+		# ---------------------------------------
+		# MULTI CHANNELS CONVOLUTION
+		# ---------------------------------------
+		# channel 1
+		inputs1 = Input(shape=(length,))
+		embedding1 = Embedding(vocab_size, 100)(inputs1)
+		conv1 = Conv1D(filters=32, kernel_size=4, activation='relu')(embedding1)
+		drop1 = Dropout(0.5)(conv1)
+		pool1 = MaxPooling1D(pool_size=2)(drop1)
+		flat1 = Flatten()(pool1)
+
+		# channel 2
+		inputs2 = Input(shape=(length,))
+		embedding2 = Embedding(vocab_size, 100)(inputs2)
+		conv2 = Conv1D(filters=32, kernel_size=6, activation='relu')(embedding2)
+		drop2 = Dropout(0.5)(conv2)
+		pool2 = MaxPooling1D(pool_size=2)(drop2)
+		flat2 = Flatten()(pool2)
+		
+		# channel 3
+		inputs3 = Input(shape=(length,))
+		embedding3 = Embedding(vocab_size, 100)(inputs3)
+		conv3 = Conv1D(filters=32, kernel_size=8, activation='relu')(embedding3)
+		drop3 = Dropout(0.5)(conv3)
+		pool3 = MaxPooling1D(pool_size=2)(drop3)
+		flat3 = Flatten()(pool3)
+		
+		# merge
+		merged = concatenate([flat1, flat2, flat3])
+		# interpretation
+		dense1 = Dense(10, activation='relu')(merged)
+		outputs = Dense(1, activation='sigmoid')(dense1)
+		model = Model(inputs=[inputs1, inputs2, inputs3], outputs=outputs)
+		
+		# compile
+		model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+		
+		# summarize
+		print(model.summary())
+		plot_model(model, show_shapes=True, to_file='multichannel.png')
+
+		# TRAIN
+		#model.fit([trainX,trainX,trainX], array(trainLabels), epochs=10, batch_size=16)
 
 		# ---------------------
 		# ! ONLY FOR LSTM MODEL
