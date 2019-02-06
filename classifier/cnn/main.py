bf19abb6890bfde4ebfc4a743716121bf17d3e44
Laurent Vanni <lvanni@unice.fr>
2019-02-06 15:15:31 +0100

    NEW: multi_channels model is working TODO: need to work on the data loading

--- classifier/cnn/main.py
+++ classifier/cnn/main.py
@@ -23,20 +23,34 @@ class PreProcessing:
 		
 		label_dic = {}
 		labels = []
-		texts = []
+		texts = [[]]*3
 		
 		# Read text and detect classes/labels
 		self.num_classes = 0
 		f = open(corpus_file, "r")
 		for text in f.readlines():
+
+			# LABELS
 			label = text.split("__ ")[0].replace("__", "")
-			text = text.replace("__" + label + "__ ", "")
 			if label not in label_dic.keys():
 				label_dic[label] = self.num_classes
 				self.num_classes += 1
 			label_int = label_dic[label]
 			labels += [label_int]
-			texts += [text]
+
+			# TEXT
+			text = text.replace("__" + label + "__ ", "")
+			sentence = [""]*3
+			for token in text.split():
+				args = token.split("**")
+				for i, arg in enumerate(args):
+					sentence[i] += arg + " "
+			for i in range(3):
+				sentence[i] += "\n"
+				texts[i] += [sentence[i]]
+
+			#print(texts)
+
 		f.close()
 		
 		print("DETECTED LABELS :")
@@ -119,33 +133,35 @@ def train(corpus_file, model_file, config):
 	# Establish params
 	config["num_classes"] = preprocessing.num_classes 
 	config["vocab_size"] = len(preprocessing.my_dictionary["word_index"]) 
+	config["F_vocab_size"] = config["vocab_size"]
+	config["C_vocab_size"] = config["vocab_size"]
+	config["L_vocab_size"] = config["vocab_size"]
 
 	# create and get model
 	cnn_model = models.CNNModel()
-	model, deconv_model, attention_model = cnn_model.getModel(config=config, weight=preprocessing.embedding_matrix)
+	model, deconv_model = cnn_model.getModel(config=config, weight=preprocessing.embedding_matrix)
 
 	# train model
 	x_train, y_train, x_val, y_val = preprocessing.x_train, preprocessing.y_train, preprocessing.x_val, preprocessing.y_val
 	checkpoint = ModelCheckpoint(model_file, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-	#checkpoint = ModelCheckpoint(model_file, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 	callbacks_list = [checkpoint]
-	model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=config["NUM_EPOCHS"], batch_size=config["BACH_SIZE"], callbacks=callbacks_list)
+	model.fit([x_train,x_train,x_train], y_train, validation_data=([x_val, x_val,x_val], y_val), epochs=config["NUM_EPOCHS"], batch_size=config["BACH_SIZE"], callbacks=callbacks_list)
 
+	"""
 	try:
-		# SETUP THE DECONV LAYER WEIGHTS
-		for layer in deconv_model.layers:	
-			if type(layer) is Conv2D:
-				deconv_weights = layer.get_weights()[0]
-		deconv_bias = deconv_model.layers[-1].get_weights()[1]
-		deconv_model.layers[-1].set_weights([deconv_weights, deconv_bias])
+		for deconv in deconv_model:
+			# SETUP THE DECONV LAYER WEIGHTS
+			for layer in deconv_model.layers:	
+				if type(layer) is Conv2D:
+					deconv_weights = layer.get_weights()[0]
+			deconv_bias = deconv_model.layers[-1].get_weights()[1]
+			deconv_model.layers[-1].set_weights([deconv_weights, deconv_bias])
 	except:
 		print("WARNING: not convolution in this model!")
+	"""
 
 	# save deconv model
-	deconv_model.save(model_file + ".deconv")
-
-	# save attention model
-	attention_model.save(model_file + ".attention")
+	deconv_model[0].save(model_file + ".deconv")
 
 	# get score
 	model = load_model(model_file)
@@ -155,6 +171,11 @@ def train(corpus_file, model_file, config):
 def predict(text_file, model_file, config, vectors_file):
 
 	result = []
+
+	# GIF ANIMATION
+	raw_images = []
+	rgb_images = []
+	final_images = []
 
 	# preprocess data
 	preprocessing = PreProcessing()
@@ -175,6 +196,7 @@ def predict(text_file, model_file, config, vectors_file):
 	# load deconv_model
 	deconv_model = load_model(model_file + ".deconv")
 	
+	"""
 	try:
 		# SETUP THE DECONV LAYER WEIGHTS
 		for layer in deconv_model.layers:	
@@ -188,9 +210,11 @@ def predict(text_file, model_file, config, vectors_file):
 	# apply deconvolution
 	deconv = deconv_model.predict(x_data)
 	print("deconvolution", 	deconv.shape)
+	"""
 
 	my_dictionary = preprocessing.my_dictionary
 
+	"""
 	print("----------------------------")
 	print("ATTENTION")
 	print("----------------------------")
@@ -203,14 +227,9 @@ def predict(text_file, model_file, config, vectors_file):
 
 	print("attentions", attentions.shape)	
 
-	# GIF ANIMATION
-	raw_images = []
-	rgb_images = []
-	final_images = []
-
 	# Format result (prediction + deconvolution)
 	my_dictionary = preprocessing.my_dictionary
-
+	"""
 	for sentence_nb in range(len(x_data)):
 		sentence = {}
 		sentence["sentence"] = ""
@@ -222,22 +241,26 @@ def predict(text_file, model_file, config, vectors_file):
 			# Normalize deconv values
 			j = int(config["EMBEDDING_DIM"]/3)
 
-			deconv_part = [np.copy(deconv[sentence_nb][:,:j]), np.copy(deconv[sentence_nb][:,j:j+j]), np.copy(deconv[sentence_nb][:,-j:])]
+			#deconv_part = [np.copy(deconv[sentence_nb][:,:j]), np.copy(deconv[sentence_nb][:,j:j+j]), np.copy(deconv[sentence_nb][:,-j:])]
 			deconv_values = {}
 
 			# ------------------------
 			# Normalize values
 			# Very slow operation
 			# TODO: Optimize this part
+			"""
 			for part_nb, part in enumerate(["CODE", "LEMME", "FORME"]):
 				ratio = 255 / np.max(deconv_part[part_nb])
 				deconv_values[part] = deconv_part[part_nb]
 				deconv_values[part] = np.multiply(deconv_values[part], [ratio])
+			"""
 			# ------------------------
 			
+			"""
 			forme_values = deconv_values["FORME"]
 			code_values = deconv_values["CODE"]
 			lemme_values = deconv_values["LEMME"]
+			"""
 
 			# Create word entry
 			for i in range(config["SEQUENCE_SIZE"]):
@@ -245,6 +268,7 @@ def predict(text_file, model_file, config, vectors_file):
 				word = my_dictionary["index_word"].get(index, "PAD")
 			
 				# READ ATTENTION 
+				"""
 				if i == 0 or i == len(x_data[sentence_nb])-1: # because shape (?,48,1)
 					attention_value = 0
 				else:
@@ -252,9 +276,12 @@ def predict(text_file, model_file, config, vectors_file):
 						attention_value = attentions[sentence_nb][i-1]
 					except: # BUG WITH FILTER_SIZE > 3
 						attention_value = 0
+				"""
 
 				# WRITE WORD ENTRY
 				word_args = word.split("**")
+
+				"""
 				# deconvolution forme
 				word = word_args[0] + "*" + str(np.sum(forme_values[i]))
 				# deconvolution code
@@ -266,7 +293,13 @@ def predict(text_file, model_file, config, vectors_file):
 				except:
 					pass # PAD VALUE
 				word += "*" + str(float(attention_value))
+				"""
 
+				word = word_args[0] + "*1**" 
+				word += word_args[1] + "*1**"
+				word += word_args[2] + "*1"
+				# attention
+				word += "*1"
 				sentence["sentence"] += word + " "
 		
 		# ------ STANDARD VERSION -------
@@ -276,9 +309,10 @@ def predict(text_file, model_file, config, vectors_file):
 				word = my_dictionary["index_word"].get(index, "PAD")
 
 				# READ DECONVOLUTION 
-				forme_values = deconv[sentence_nb][i]
+				#forme_values = deconv[sentence_nb][i]
 				
 				# READ ATTENTION 
+				"""
 				if i == 0 or i == len(x_data[sentence_nb])-1: # because shape (?,48,1)
 					attention_value = 0
 				else:
@@ -286,18 +320,20 @@ def predict(text_file, model_file, config, vectors_file):
 						attention_value = attentions[sentence_nb][i-1]
 					except: # BUG WITH FILTER_SIZE > 3
 						attention_value = 0
+				"""
 
 				# WRITE WORD ENTRY
 				# deconvolution
-				word = word + "*" + str(np.sum(forme_values))
+				word = word + "*1"
 				# attention
-				word += "*" + str(float(attention_value))
+				word += "*1"
 
 				sentence["sentence"] += word + " "
 
 		result.append(sentence)
 
 		# ------ DRAW DECONV FACE ------
+		"""
 		raw_image = np.zeros( (config["SEQUENCE_SIZE"], config["EMBEDDING_DIM"], 3), dtype=np.uint8 )
 		rgb_image = np.zeros( (config["SEQUENCE_SIZE"], config["EMBEDDING_DIM"], 3), dtype=np.uint8 )
 		final_image = np.zeros( (config["SEQUENCE_SIZE"], config["EMBEDDING_DIM"], 3), dtype=np.uint8 )
@@ -354,6 +390,7 @@ def predict(text_file, model_file, config, vectors_file):
 	imageio.mimsave(model_file + "_raw.gif", raw_images, duration=0.1)
 	imageio.mimsave(model_file + "_rgb.gif", rgb_images, duration=0.1)
 	imageio.mimsave(model_file + "_final.gif", final_images, duration=0.1)
+	"""
 
 	return result
 
--- classifier/cnn/models.py
+++ classifier/cnn/models.py
@@ -16,6 +16,8 @@ from keras.layers import Permute
 from keras.layers import Lambda
 from keras.layers import Conv1D, Conv2D, Conv2DTranspose, MaxPooling1D, MaxPooling2D, GlobalMaxPooling1D,  Embedding, Reshape
 from keras.layers import Input, Embedding, LSTM, Dense
+from keras.layers import Lambda
+from keras.layers import concatenate
 from keras.utils import np_utils
 from keras.layers import multiply
 
@@ -75,111 +77,51 @@ class CNNModel:
 		# ---------------------------------------
 		# MULTI CHANNELS CONVOLUTION
 		# ---------------------------------------
-		# channel 1
-		inputs1 = Input(shape=(length,))
-		embedding1 = Embedding(vocab_size, 100)(inputs1)
-		conv1 = Conv1D(filters=32, kernel_size=4, activation='relu')(embedding1)
-		drop1 = Dropout(0.5)(conv1)
-		pool1 = MaxPooling1D(pool_size=2)(drop1)
-		flat1 = Flatten()(pool1)
+		inputs = [0]*3
+		embedding = [0]*3
+		reshape = [0]*3
+		conv = [0]*3
+		deconv = [0]*3
+		deconv_model = [0]*3
+		pool = [0]*3
+		flat = [0]*3
 
-		# channel 2
-		inputs2 = Input(shape=(length,))
-		embedding2 = Embedding(vocab_size, 100)(inputs2)
-		conv2 = Conv1D(filters=32, kernel_size=6, activation='relu')(embedding2)
-		drop2 = Dropout(0.5)(conv2)
-		pool2 = MaxPooling1D(pool_size=2)(drop2)
-		flat2 = Flatten()(pool2)
-		
-		# channel 3
-		inputs3 = Input(shape=(length,))
-		embedding3 = Embedding(vocab_size, 100)(inputs3)
-		conv3 = Conv1D(filters=32, kernel_size=8, activation='relu')(embedding3)
-		drop3 = Dropout(0.5)(conv3)
-		pool3 = MaxPooling1D(pool_size=2)(drop3)
-		flat3 = Flatten()(pool3)
+		for i, arg in enumerate(["F", "C", "L"]):
+			print("CHANNELS ", i)
+			inputs[i] = Input(shape=(config["SEQUENCE_SIZE"],), dtype='int32')
+			print("input", i,  inputs[i].shape)
+			embedding[i] = Embedding(
+				config[arg + "_vocab_size"]+1,
+				config[arg + "_EMBEDDING_DIM"],
+				input_length=config["SEQUENCE_SIZE"],
+				weights=weight,
+				trainable=True
+			)(inputs[i])
+			print("embedding", i,  embedding[i].shape)
+			reshape[i] = Reshape((config["SEQUENCE_SIZE"], config[arg + "_EMBEDDING_DIM"], 1))(embedding[i])
+			print("reshape", i,  reshape[i].shape)
+			conv[i] = Conv2D(config[arg + "_NB_FILTERS"], (config[arg + "_FILTER_SIZES"], config[arg + "_EMBEDDING_DIM"]), padding='valid', kernel_initializer='normal', activation='relu', data_format='channels_last')(reshape[i])
+			print("conv", i,  conv[i].shape)
+			deconv[i] = Conv2DTranspose(1, (config[arg + "_FILTER_SIZES"], config[arg + "_EMBEDDING_DIM"]), padding='valid', kernel_initializer='normal', activation='relu', data_format='channels_last')(conv[i])
+			deconv_model[i] = Model(inputs=inputs[i], outputs=deconv[i])
+			pool[i] = MaxPooling2D(pool_size=(config["SEQUENCE_SIZE"] - config[arg + "_FILTER_SIZES"] + 1, 1), strides=(1, config[arg + "_EMBEDDING_DIM"]), padding='valid', data_format='channels_last')(conv[i])
+			print("pool", i,  pool[i].shape)
+			flat[i] = Flatten()(pool[i])
+			print("flat", i,  flat[i].shape)
+			print("-"*20)
 		
 		# merge
-		merged = concatenate([flat1, flat2, flat3])
-		# interpretation
-		dense1 = Dense(10, activation='relu')(merged)
-		outputs = Dense(1, activation='sigmoid')(dense1)
-		model = Model(inputs=[inputs1, inputs2, inputs3], outputs=outputs)
-		
-		# compile
-		model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-		
-		# summarize
-		print(model.summary())
-		plot_model(model, show_shapes=True, to_file='multichannel.png')
+		merged = concatenate([flat[0], flat[1], flat[2]])
+		print("merged", merged.shape)
 
-		# TRAIN
-		#model.fit([trainX,trainX,trainX], array(trainLabels), epochs=10, batch_size=16)
-
-		# ---------------------
-		# ! ONLY FOR LSTM MODEL
-		# ---------------------		
-		conv_shape = conv.shape[1:]
-		reshape = Reshape((int(conv_shape[0]),int(np.prod(conv_shape[1:]))))(conv)
-		print("reshape :", reshape.shape)
-
-		# ----------
-		# LSTM LAYER
-		# ----------
-		# Select input as "reshape" to use CONVOLUTION
-		# Select input as "embedding" to drop CONVOLUTION
-		if (config["ENABLE_CONV"]):
-			lstm = LSTM(config["LSTM_SIZE"], return_sequences=True)(reshape) #(embedding) <=== Select here with or without convolution
-		else:
-			lstm = LSTM(config["LSTM_SIZE"], return_sequences=True)(embedding)
-		print("lstm :", lstm.shape)
-
-		# ---------------
-		# ATTENTION LAYER
-		# ---------------
-		attention = TimeDistributed(Dense(1, activation='tanh'))(lstm) 
-		print("TimeDistributed :", attention.shape)
-
-		# reshape Attention
-		attention = Flatten()(attention)
-		print("Flatten :", attention.shape)
-		
-		attention = Activation('softmax')(attention)
-		print("Activation :", attention.shape)
-
-		# Observe attention here
-		attention_model = Model(inputs=inputs, outputs=attention)
-
-		# Pour pouvoir faire la multiplication (scalair/vecteur KERAS)
-		attention = RepeatVector(config["LSTM_SIZE"])(attention)
-		print("RepeatVector :", attention.shape)
-		
-		attention = Permute([2, 1])(attention)
-		print("Permute :", attention.shape)
-
-		# apply the attention		
-		sent_representation = multiply([lstm, attention])
-		print("Multiply :", sent_representation.shape)
-		
-		sent_representation = Lambda(lambda xin: K.sum(xin, axis=1))(sent_representation)
-		print("Lambda :", sent_representation.shape)
-
-		# -------------
-		# DROPOUT LAYER
-		# -------------
-		# Select input as "sent_representation" to use LSTM
-		# Select input as "maxpool" to drop LSTM
-		if config["ENABLE_LSTM"]:
-			dropout = Dropout(config["DROPOUT_VAL"])(sent_representation)
-		else:
-			dropout = Dropout(config["DROPOUT_VAL"])(maxpool)
+		# dropout
+		dropout = Dropout(config["DROPOUT_VAL"])(merged)
 		print("Dropout :", dropout.shape)
-
 
 		# -----------------
 		# HIDDEN DENSE LAYER
 		# -----------------		
-		hidden_dense = Dense(config["DENSE_LAYER_SIZE"],kernel_initializer='uniform',activation='relu')(dropout)
+		hidden_dense = Dense(config["DENSE_LAYER_SIZE"], kernel_initializer='uniform',activation='relu')(dropout)
 
 		# -----------------
 		# FINAL DENSE LAYER
@@ -204,11 +146,10 @@ class CNNModel:
 		print("TRAINING MODEL")
 		print(model.summary())
 
-		print("DECONV MODEL")
-		print(deconv_model.summary())
+		#print("DECONV MODEL")
+		#print(deconv_model.summary())
 
-		print("ATTENTION MODEL")
-		print(attention_model.summary())
+		#print("ATTENTION MODEL")
+		#print(attention_model.summary())
 
-		return model, deconv_model, attention_model
-	
+		return model, deconv_model
