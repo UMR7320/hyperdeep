3103a9cec1e0216461fc71e63bd3562dfc71f96c
Laurent Vanni <lvanni@unice.fr>
2019-02-13 13:21:03 +0100

    FIX: typo

--- skipgram/skipgram_with_NS.py
+++ skipgram/skipgram_with_NS.py
@@ -18,7 +18,7 @@ def create_vectors(corpus_file, model_file, config, nb_channels):
         sentences = gensim.models.word2vec.LineSentence(corpus_file + "." + str(i))
 
         # sg defines the training algorithm. By default (sg=0), CBOW is used. Otherwise (sg=1), skip-gram is employed.
-        model = gensim.models.Word2Vec(sentences, size=config["EMBEDDING_DIM"], window=window, min_count=config["MIN_COUNT"], workers=8, sg=config["SG"])
+        model = gensim.models.Word2Vec(sentences, size=config["EMBEDDING_DIM"], window=config["WINDOW_SIZE"], min_count=config["MIN_COUNT"], workers=8, sg=config["SG"])
 
         f = open(model_file + "." + str(i) + ".vec"  ,'w')
         vectors = []
