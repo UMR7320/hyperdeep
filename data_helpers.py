da0eab055431bcc48510f496650f0ca6a5992d38
Laurent Vanni <lvanni@unice.fr>
2019-10-03 12:06:13 +0200

    FIX: multichannel word[0].isupper()

--- data_helpers.py
+++ data_helpers.py
@@ -21,26 +21,26 @@ def tokenize(texts, model_file, create_dictionnary, config):
 	datas = []		
 
 	type = ["FORME", "CODE", "LEM"]
-	for i, text in texts.items():
+	for channel, text in texts.items():
 		datas += [(np.zeros((len(text), config["SEQUENCE_SIZE"]))).astype('int32')]
 
 		line_number = 0
-		for line in text:
+		for i, line in enumerate(text):
 			words = line.split()[:config["SEQUENCE_SIZE"]]
 			sentence_length = len(words)
 			sentence = []
-			for word in words:
-				if word not in dictionaries[i]["word_index"].keys():
+			for j, word in enumerate(words):
+				if word not in dictionaries[channel]["word_index"].keys():
 					if create_dictionnary:
 						# FILTERS
 						# not a number and len > 1
-						skip_word = word.isdigit() or len(word) == 1 or word[0].isupper()
+						skip_word = word.isdigit() or len(word) or text[0][i][j]
 						if not skip_word: # f > k+2%
 							for spec in config["Z_SCORE"].values():
 								try:
-									test_k = spec[type[i]][word]["k"] + (spec[type[i]][word]["k"]*0.1)
-									if spec[type[i]][word]["f"] < test_k or spec[type[i]][word]["f"] < 10:
-										print("skip : ", word, spec[type[i]][word]["f"] , " < ", spec[type[i]][word]["k"])
+									test_k = spec[type[channel]][word]["k"] + (spec[type[channel]][word]["k"]*0.1)
+									if spec[type[channel]][word]["f"] < test_k or spec[type[channel]][word]["f"] < 10:
+										print("skip : ", word, spec[type[channel]][word]["f"] , " < ", spec[type[channel]][word]["k"])
 										skip_word = True
 										break
 								except :
@@ -48,26 +48,26 @@ def tokenize(texts, model_file, create_dictionnary, config):
 
 						# IF WORD IS SKIPED THEN ADD "UK" word
 						if skip_word: 
-							dictionaries[i]["word_index"][word] = dictionary["word_index"]["UK"]
+							dictionaries[channel]["word_index"][word] = dictionary["word_index"]["UK"]
 						else:	 
-							indexes[i] += 1
-							dictionaries[i]["word_index"][word] = indexes[i]
-							dictionaries[i]["index_word"][indexes[i]] = word
+							indexes[channel] += 1
+							dictionaries[channel]["word_index"][word] = indexes[channel]
+							dictionaries[channel]["index_word"][indexes[channel]] = word
 
 					else:        
 						# FOR UNKNOWN WORDS
 						try:
-							dictionaries[i]["word_index"][word] = dictionaries[i]["word_index"]["UK"]
+							dictionaries[channel]["word_index"][word] = dictionaries[channel]["word_index"]["UK"]
 						except:
-							dictionaries[i]["word_index"][word] = dictionaries[i]["word_index"]["PAD"]
-				sentence.append(dictionaries[i]["word_index"][word])
+							dictionaries[channel]["word_index"][word] = dictionaries[channel]["word_index"]["PAD"]
+				sentence.append(dictionaries[channel]["word_index"][word])
 
 			# COMPLETE WITH PAD IF LENGTH IS < SEQUENCE_SIZE
 			if sentence_length < config["SEQUENCE_SIZE"]:
 				for j in range(config["SEQUENCE_SIZE"] - sentence_length):
-					sentence.append(dictionaries[i]["word_index"]["PAD"])
+					sentence.append(dictionaries[channel]["word_index"]["PAD"])
 			
-			datas[i][line_number] = sentence
+			datas[channel][line_number] = sentence
 			line_number += 1
 
 	if create_dictionnary:
